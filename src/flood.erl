f0d072d9ed88a44436ef3712c0b215ac19ef9944
Kajetan Rzepecki <kajetan.rzepecki@gmail.com>
2013-05-21 10:37:20 +0200

    Added API forwards from flood app to flood_serv

--- src/flood.erl
+++ src/flood.erl
@@ -1,7 +1,11 @@
 -module(flood).
+
 -export([start/0, stop/0]).
 
 -export([get_env/1]).
+
+-export([spawn_clients/1, spawn_clients/2, kill_clients/1, disconnect_clients/1]).
+-export([clients_status/0, clients_status/1, ping/0]).
 
 start() ->
     lager:start(),
@@ -13,3 +17,24 @@ stop() ->
 get_env(Key) ->
     {ok, Value} = application:get_env(?MODULE, Key),
     Value.
+
+spawn_clients(Number) ->
+    flood_serv:spawn_clients(Number).
+
+spawn_clients(Number, Args) ->
+    flood_serv:spawn_clients(Number, Args).
+
+kill_clients(Number) ->
+    flood_serv:kill_clients(Number).
+
+disconnect_clients(Number) ->
+    flood_serv:disconnect_clients(Number).
+
+clients_status() ->
+    flood_serv:clients_status().
+
+clients_status(Strategy) ->
+    flood_serv:clients_status(Strategy).
+
+ping() ->
+    flood_serv:ping().
